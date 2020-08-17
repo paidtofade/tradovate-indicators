cbcaf64855512fce4bfd54ead3c8801f5735aaeb
paidtofade <paidtofade@users.noreply.github.com>
2020-08-17 08:25:19 -0500

    Put in GPLv3 text

--- rangeBoxes/p2fRangeBoxes.js
+++ rangeBoxes/p2fRangeBoxes.js
@@ -1,8 +1,19 @@
-// Created by @paidtofade in 2020 
+// Created by @paidtofade in 2020
 //
+// If you find this indicator helpful, give me some love on twitter @paidtofade
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 const meta = require("./tools/meta");
 const predef = require("./tools/predef");
